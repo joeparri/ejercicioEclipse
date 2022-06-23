797d1ca787cf11010854ed4a9439fc92a7c24dfd
joeparri <35254409+joeparri@users.noreply.github.com>
2022-06-23 11:54:49 +0200

    Update MiClase.java

--- src/ejercicioEclipse/MiClase.java
+++ src/ejercicioEclipse/MiClase.java
@@ -1,10 +1,13 @@
+/**
+ * 01EC4EF4AD50E3A884A8C165DE22299A3F9218C1EE80F1866AACEAB4B53FF7DD
+ */
 package ejercicioEclipse;
 
 public class MiClase {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hola, Qué tal?, me has escuchado bien? Muy bien");
+		System.out.println("Hola, QuÃ© tal?, me has escuchado bien? Muy bien");
 
 	}
 }
